9efefba4f25cd54215e1f6da1b724e4c524fce71
Kangwook Lee <kangwooklee29@gmail.com>
2023-09-04 20:24:46 +0900

    Check if the recently authorized user is joined to the service #16 (#17)
    Changelog
    ====
    - Improves stability by adding error handling in
    `create_categories_for_user`.
    - Introduces `/get_username` endpoint to retrieve the logged-in user's
    name, enhancing the user status display on `index-authorized.html`.
    - Implementes ID guest, PW guest login feature on `index-guest.html` to
    ease service usage.
    - Adds code in `/login/callback` endpoint to handle the `guest=true`
    query parameter.
    - Implements `check_if_joined` function to verify sign-up status based
    on the presence of category records for a given user_id.
    - Creats `/join` endpoint and `join.html` page, prompting non-registered
    users to consent to joining upon login.

--- flask_server/models.py
+++ flask_server/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm.exc import NoResultFound
 
 DEFAULT_CATEGORIES = ['All', 'None', 'Deleted']
 
@@ -18,6 +19,23 @@ class Message(db.Model):
     parent_msg_id = db.Column(db.Integer)
 
 def create_categories_for_user(user_id):
-    for category_name in DEFAULT_CATEGORIES:
-        db.session.add(Category(name=category_name, user_id=user_id))
-    db.session.commit()
+    try:
+        for category_name in DEFAULT_CATEGORIES:
+            db.session.add(Category(name=category_name, user_id=user_id))
+        db.session.commit()
+        return True
+    except SQLAlchemyError:
+        db.session.rollback()
+        return False
+
+def check_if_joined(user_id):
+    try:        
+        if db.session.query(Category).filter_by(user_id=user_id).first():
+            return True
+        else:
+            return False
+    except NoResultFound:
+        return False
+    except SQLAlchemyError:
+        db.session.rollback()
+        return False
