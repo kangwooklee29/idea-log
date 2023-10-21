a8a3f423609d50ebb9fb46965650ee90ed96f4b1
kangwooklee29 <kangwooklee29@gmail.com>
2023-10-21 19:39:04 +0000

    Initial commit for #77

--- flask_server/app.py
+++ flask_server/app.py
@@ -8,6 +8,7 @@ from authlib.integrations.flask_client import OAuth
 from decouple import config
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from werkzeug.middleware.proxy_fix import ProxyFix
 from flask_server import models
@@ -16,7 +17,7 @@ from .blueprints import blueprint, blueprint_auth, blueprint_api
 STATIC_FOLDER = "../web_client"
 
 
-def create_app():
+def create_app(is_test: bool = False):
     """
     Create a Flask app
 
@@ -28,23 +29,25 @@ def create_app():
                       static_folder=STATIC_FOLDER)
     flask_app.config[
         'SESSION_COOKIE_SECURE'] = True  # allow HTTPS only for session cookie
-    CORS(flask_app,
-         origins=[config('DOMAIN_NAME')
-                  ])  # allow requests only from the configured domain name
-    flask_app.secret_key = config('FLASK_SECRET_KEY')
-    flask_app.wsgi_app = ProxyFix(flask_app.wsgi_app, x_proto=1)
-    flask_app.google_oauth = OAuth(flask_app).register(
-        name='google',
-        client_id=config('GOOGLE_OAUTH_CLIENT_ID'),
-        client_secret=config('GOOGLE_OAUTH_CLIENT_SECRET'),
-        authorize_url='https://accounts.google.com/o/oauth2/auth',
-        authorize_params=None,
-        access_token_url='https://accounts.google.com/o/oauth2/token',
-        access_token_method='POST',
-        refresh_token_url=None,
-        redirect_to='authorized',
-        client_kwargs={'scope': 'profile email'},
-    )
+    if is_test is False:
+        CORS(flask_app,
+             origins=[config('DOMAIN_NAME')
+                      ])  # allow requests only from the configured domain name
+        flask_app.secret_key = config('FLASK_SECRET_KEY')
+        flask_app.wsgi_app = ProxyFix(  # type: ignore
+            flask_app.wsgi_app, x_proto=1)
+        flask_app.google_oauth = OAuth(flask_app).register(  # type: ignore
+            name='google',
+            client_id=config('GOOGLE_OAUTH_CLIENT_ID'),
+            client_secret=config('GOOGLE_OAUTH_CLIENT_SECRET'),
+            authorize_url='https://accounts.google.com/o/oauth2/auth',
+            authorize_params=None,
+            access_token_url='https://accounts.google.com/o/oauth2/token',
+            access_token_method='POST',
+            refresh_token_url=None,
+            redirect_to='authorized',
+            client_kwargs={'scope': 'profile email'},
+        )
     flask_app.register_blueprint(blueprint, url_prefix='')
     flask_app.register_blueprint(blueprint_auth, url_prefix='/auth')
     flask_app.register_blueprint(blueprint_api, url_prefix='/api')
@@ -52,17 +55,16 @@ def create_app():
     # Initialize db
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + config(
         'SQLALCHEMY_DATABASE_URI')
-    flask_app.db = models.db
-    flask_app.db.init_app(flask_app)
+    flask_app.db = models.db  # type: ignore
+    flask_app.db.init_app(flask_app)  # type: ignore
 
     # Create tables if they do not exist already
     with flask_app.app_context():
-        flask_app.db.create_all()
+        flask_app.db.create_all()  # type: ignore
 
     return flask_app
 
 
-app = create_app()
-
 if __name__ == '__main__':
+    app = create_app()
     app.run(host='0.0.0.0', debug=True)
--- flask_server/tests/blueprints/api/test_views.py
+++ flask_server/tests/blueprints/api/test_views.py
@@ -2,6 +2,7 @@
 flask_server/tests/blueprints/api/test_views.py
 """
 # pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
 
 from unittest.mock import patch
 import pytest
@@ -16,7 +17,7 @@ def test_client():
     Returns:
         Flask test client instance.
     """
-    app = create_app()
+    app = create_app(True)
     with app.test_client() as client:
         yield client
 
@@ -117,3 +118,67 @@ def test_fetch_categories(test_client, mock_fetch_by_user_id):
     assert data[1]['user_id'] == "test_user"
 
     mock_fetch_by_user_id.assert_called_with('test_user')
+
+
+def test_update_category(test_client, mock_fetch_by_user_id):
+    """Test updating an existing category.
+    
+    Given:
+        A category ID, name, and user ID.
+    When:
+        The '/api/update_category' endpoint is hit.
+    Then:
+        The category's name should be updated correctly.
+    """
+    with test_client.session_transaction() as sess:
+        sess['profile'] = {'id': 'test_user'}
+
+    response = test_client.get(
+        '/api/update_category?name=UpdatedCategory&id=1')
+    json_data = response.get_json()
+    assert response.status_code == 200
+    assert json_data['success'] is True
+
+    category = Category.query.get(1)
+    assert category.name == 'UpdatedCategory'
+
+
+def test_create_new_category(test_client):
+    """Test creating a new category.
+    
+    Given:
+        A name and user ID but no category ID.
+    When:
+        The '/api/update_category' endpoint is hit.
+    Then:
+        A new category should be created.
+    """
+    with test_client.session_transaction() as sess:
+        sess['profile'] = {'id': 'test_user'}
+
+    response = test_client.get('/api/update_category?name=NewCategory')
+    json_data = response.get_json()
+    assert response.status_code == 200
+    assert json_data['success'] is True
+
+    category = Category.query.filter_by(name='NewCategory').first()
+    assert category is not None
+
+
+def test_update_invalid_category(test_client):
+    """Test updating a category with an invalid ID.
+    
+    Given:
+        An invalid category ID.
+    When:
+        The '/api/update_category' endpoint is hit.
+    Then:
+        The update should fail and return success as False.
+    """
+    with test_client.session_transaction() as sess:
+        sess['profile'] = {'id': 'test_user'}
+
+    response = test_client.get('/api/update_category?name=NewCategory&id=9999')
+    json_data = response.get_json()
+    assert response.status_code == 200
+    assert json_data['success'] is False
